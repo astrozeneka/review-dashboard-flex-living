e8c475d7659ec9c51c8301cd1ee8802cbc235fc1
Ryan <Ryan@localhost.com>
2025-11-08 10:38:24 +0700

    Add basic configuration for the Next.js/React app

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,12 @@
+import { prisma } from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  const { email, name } = await request.json()
+  
+  const user = await prisma.user.create({
+    data: { email, name }
+  })
+  
+  return NextResponse.json(user)
+}
